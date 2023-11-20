f2004b037f021487e5aba851980ce55d90f1788d
JohnBryan2822 <johnbryan2822@gmail.com>
2023-11-20 22:30:01 +0100

    Currency-exchange-service - Microservice

---
+++ currency-exchange-service/src/main/java/org/javacoders/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,36 @@
+package org.javacoders.microservices.currencyexchangeservice;
+
+import java.math.BigDecimal;
+
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyExchangeController {
+	
+	private Environment environment;
+	private CurrencyExchangeRepository repository;
+	
+	public CurrencyExchangeController(Environment environment, CurrencyExchangeRepository repository) {
+		this.environment = environment;
+		this.repository = repository;
+	}
+
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyExchange retrieveExchangeValue(
+			@PathVariable String from,
+			@PathVariable String to) {
+		
+		CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
+		if(currencyExchange == null) {
+			throw new RuntimeException("Unable to find data for " + from + " to " + to);
+		}
+		
+		String port = environment.getProperty("local.server.port");
+		currencyExchange.setEnvironment(port);
+		
+		return currencyExchange;
+	}
+}
